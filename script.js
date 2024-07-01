851d9bb309d97ed7fd9bcd34d2b219ae2adf5d8f
sanchitsubedi69 <sanchit.795424@cct.tu.edu.np>
2024-07-01 15:09:18 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//console.log("bye hello aayush world")
+//var bottle ="pani"
+//let bottle ="pani"
+//const bottle ="pani" 
+const friend1 = 'Pranjal'
+const friend2 = 'Bob'
+const friend3 = 'Anup'
+
+const friends = ['pranjal','bob','anup',12,true]
+
+const me = {
+    name : 'manish basnet',
+    age: 22,
+    address : 'itahari'
+    
+}
